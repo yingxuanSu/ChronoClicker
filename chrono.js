90073e95bedee33df646c0671897f14f9d74d195
zzzOwO <106785842+zzzOwO@users.noreply.github.com>
2025-01-18 16:28:31 -0500

    Update chrono.js

--- chrono.js
+++ chrono.js
@@ -13,3 +13,26 @@ if (!birthdateInput) {
     alert('Please enter your birthdate.');
     return;
 }
+
+
+const events = [
+    {
+        date: new Date('2001-09-11'),  // Example: 9/11
+        event: 'The 9/11 attacks occurred.'
+    },
+    {
+        date: new Date('2004-02-04'),  // Example: Facebook launched
+        event: 'Facebook was launched.'
+    },
+    {
+        date: new Date('2008-11-04'),  // Example: Barack Obama elected
+        event: 'Barack Obama was elected President of the United States.'
+    },
+    {
+        date: new Date('2020-03-11'),  // Example: COVID-19 pandemic declared
+        event: 'COVID-19 pandemic was declared by WHO.'
+    },
+
+
+
+];
