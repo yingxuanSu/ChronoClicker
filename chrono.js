092ce43e59241919b640194fb190981f80e9c1bd
zzzOwO <106785842+zzzOwO@users.noreply.github.com>
2025-01-18 16:27:33 -0500

    Update chrono.js

--- chrono.js
+++ chrono.js
@@ -1,5 +1,15 @@
 let chrono = document.querySelector('.days')
 
+// Get the user's birthdate from the input
+const birthDateInput = new Date(document.getElementById('birthDate').value);
+let currentDate = birthDateInput;
 function inChrono() {
     chrono.innerHTML = parseFloat(chrono.innerHTML) + 1
+    currentDate.setDate(birthDateInput.setDate()+1);
+}
+
+// If no birthdate is selected, show an alert
+if (!birthdateInput) {
+    alert('Please enter your birthdate.');
+    return;
 }
