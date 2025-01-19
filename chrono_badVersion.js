a972b9c885ee9aa6847478635ef559e895b24a11
zzzOwO <106785842+zzzOwO@users.noreply.github.com>
2025-01-18 20:15:42 -0500

    Create chrono_badVersion.js

---
+++ chrono_badVersion.js
@@ -0,0 +1,48 @@
+let chrono = document.querySelector('.days')
+
+// Get the user's birthdate from the input
+let birthDateInput = new Date(document.getElementById('birthDate').value);
+let currentDate = birthDateInput;
+let msg_life = "There is no important event today";
+let msg_history = "There is no important event today";
+
+function inChrono() {
+    chrono.innerHTML = parseFloat(chrono.innerHTML) + 1;
+    currentDate.setDate(birthDateInput.setDate()+1);
+}
+
+const events = [
+    {
+        date: new Date('2001-09-11'),  // Example: 9/11
+        e: 'The 9/11 attacks occurred.'
+    },
+    {
+        date: new Date('2004-02-04'),  // Example: Facebook launched
+        e: 'Facebook was launched.'
+    },
+    {
+        date: new Date('2008-11-04'),  // Example: Barack Obama elected
+        e: 'Barack Obama was elected President of the United States.'
+    },
+    {
+        date: new Date('2020-03-11'),  // Example: COVID-19 pandemic declared
+        e: 'COVID-19 pandemic was declared by WHO.'
+    },
+
+]
+
+for (const ev in events){
+    if (currentDate==ev[date]){
+        msg_history = ev[e];
+    } else if (currentDate.getMonth() == birthDateInput.getMonth() && currentDate.getDay() == birthDateInput.getDay() ){
+        msg_life = "Happy Birthday!"
+    }
+}
+const leftD= document.getElementById("leftDisplay");
+leftD.textContent = msg_life;
+
+
+
+
+
+
