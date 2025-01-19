4112cd0daa66a4f0a11d908a6ba2c29cb3d5b353
zzzOwO <106785842+zzzOwO@users.noreply.github.com>
2025-01-18 20:16:27 -0500

    Create bar1_badVersion.js

---
+++ bar1_badVersion.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Output Page</title>
+    
+</head>
+<body>
+
+    <div id="leftDisplay">
+
+    <script src="chrono.js">
+        
+    </script></div>
+
+</body>
+</html>
